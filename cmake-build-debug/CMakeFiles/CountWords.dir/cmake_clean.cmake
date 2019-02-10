b7c0ffa0206de1b2d4bc3f6782c2589aef3dab08
Andrew Obukhov <andrewrcr@mail.ru>
2019-02-10 13:49:54 +0300

    First

---
+++ cmake-build-debug/CMakeFiles/CountWords.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CountWords.dir/main.cpp.obj"
+  "CountWords.pdb"
+  "CountWords.exe"
+  "CountWords.exe.manifest"
+  "libCountWords.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CountWords.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
